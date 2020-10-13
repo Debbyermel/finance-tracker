febee5c1cf21bec846e8cc303cc37d47c009cbf6
Debora Ermel <debbyermel@gmail.com>
2020-10-13 08:50:24 -0500

    User friendships back-end

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :friendships
+  has_many :friends, through: :friendships
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
