1d883a185e582ba8113fb23ac57a554154ee07ab
Debora Ermel <debbyermel@gmail.com>
2020-10-19 09:16:36 -0500

    Fixes my portfolio stocks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
@@ -7,26 +9,28 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
-  def search_friends
-    render json: params[:friend]
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 
   def search
     if params[:friend].present?
-      @friend = params[:friend]
-      if @friend
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
         respond_to do |format|
           format.js { render partial: 'users/friend_result' }
         end
       else
         respond_to do |format|
-          flash.now[:alert] = 'Cannot find search.'
+          flash.now[:alert] = "Couldn't find user"
           format.js { render partial: 'users/friend_result' }
         end
       end
     else
       respond_to do |format|
-        flash[:alert] = 'Please enter a name our email to search.'
+        flash.now[:alert] = "Please enter a friend name or email to search"
         format.js { render partial: 'users/friend_result' }
       end
     end
